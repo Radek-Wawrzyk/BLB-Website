d9c8de65b61a7bf58bca0e015ff1ad72e21be50e
Radek <radek511@op.pl>
2019-09-24 23:58:05 +0200

    Added - Basic structure of Home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,55 +1,60 @@
 <template>
-  <section class="container">
-    <h1>Test</h1>
-    <h2>Test</h2>
-    <h3>Test</h3>
-    <h4>Test</h4>
-    <h5>Test</h5>
-    <h6>Test</h6>
-    <p>Test</p>
-  </section>
+  <main>
+    <header class="header">
+      <h1>Test</h1>
+      <h2>Test</h2>
+      <h3>Test</h3>
+      <h4>Test</h4>
+      <h5>Test</h5>
+      <h6>Test</h6>
+      <p>Test</p>
+    </header>
+    <section class="section" id="upcoming">
+      upcoming matches
+    </section>
+    <section class="section" id="about">
+      about league
+    </section>
+     <section class="section" id="stats">
+      stats
+    </section>
+    <section class="section" id="news">
+      latest news
+    </section>
+    <section class="section" id="sponsors">
+      sponsors
+    </section>
+  </main>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
 export default {
-  components: {
-    Logo
-  }
-}
+  name: 'Homepage'
+
+};
+
 </script>
 
 <style lang="scss" scoped>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+.header {
+  margin: 70px 0 0 0;
+  height: calc(100vh - 70px);
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
 }
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-  color: $primary;
+.section {
+  width: 100%;
+  height: 500px;
+  text-align: center;
+  background: $bck-primary;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.section:nth-child(even) {
+  background: $bck-secondary;
 }
 </style>
